eb2748f3356fa2893c2937f814b0e35e6f264595
raphachegeke <raphachegekamunu@gmail.com>
2025-05-27 14:38:31 +0300

    updated PWA

---
+++ static/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'ai-chat-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
